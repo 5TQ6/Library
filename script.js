334170a254ca45fdf1c9b90a1724941a66cf3a92
5tq6 <hemazoom61@gmail.com>
2022-10-06 14:13:43 +0300

    rendering books on the grid

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let myLibrary = [];
 var addbtn = document.getElementById('addBtn')
 var overlay = document.getElementById('overlay')
 var modal = document.getElementById('modal')
+var formInputs = document.getElementsByClassName('formInputs')
+var books = document.querySelector('.booksGrid')
+var readStat = document.querySelector('.notRead')
+
+
+let myLibrary = [{
+    title: 'book1',
+    author: 'me',
+    pages: 500,
+    isRead: true
+},{
+    title: 'book2',
+    author: 'u',
+    pages: 5030,
+    isRead: false
+}];
 
 addbtn.onclick = function() {
     overlay.classList.toggle('active')
@@ -13,10 +28,51 @@ overlay.onclick = function() {
     modal.classList.toggle('active')
 }
 
-function Book() {
 
+
+function Book(author,title,pages,isRead) {
+   
 }
 
-function addBookToLibrary(author,title,pages,isRead) {
-
+function addBookToLibrary() {
+    
 }
+
+function createBookElement(el, content, className) {
+    const element = document.createElement(el);
+    element.textContent = content;
+    element.setAttribute('class', className);
+    return element;
+}
+
+function createBookItem(book, index) {
+    const bookItem = document.createElement('div')
+    bookItem.setAttribute('id', index)
+    bookItem.setAttribute('key', index)
+    bookItem.setAttribute('class', 'book')
+    bookItem.appendChild(
+        createBookElement('p', `Title: ${book.title}`, "title")
+    )
+    bookItem.appendChild(
+        createBookElement('p', `author: ${book.author}`, "author")
+    )
+    bookItem.appendChild(
+        createBookElement('p', `pages: ${book.pages}`, "pages")
+    )
+    bookItem.appendChild(
+        createBookElement('button', 'Not read', "notRead")
+    )
+    bookItem.appendChild(
+        createBookElement('button', 'Remove', "remove")
+    )
+    
+    books.insertAdjacentElement('afterbegin', bookItem);
+}
+
+function renderBooks() {
+    myLibrary.map( (book,index) => {
+        createBookItem(book,index)
+    } )
+}
+
+renderBooks();
